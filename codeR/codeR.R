77fbb0cc56297f36c18c4703a10bc7a583004c29
hashtar <code>
2015-04-30 16:18:46 +0200

    ok

--- codeR/codeR.R
+++ codeR/codeR.R
@@ -44,6 +44,30 @@ h2d=hist2d(Rx/n,Ry/n,show=FALSE, same.scale=TRUE, nbins=c(10,10))
 persp(h2d$x,h2d$y,h2d$counts,col="lightblue",theta=50)
 #hist3D(h2d$x,h2d$y,h2d$counts)
 
+my_hist3d <- function(x, y, freq=FALSE, nclass="auto") {
+  n<-length(x)
+  if (nclass == "auto") { nclass<-ceiling(sqrt(nclass.Sturges(x))) }
+  breaks.x <- seq(min(x),max(x),length=(nclass+1))
+  breaks.y <- seq(min(y),max(y),length=(nclass+1))
+  h <- NULL
+  for (i in 1:nclass) 
+    for (j in 1:nclass) 
+      h <- c(h, sum(x <= breaks.x[j+1] & x >= breaks.x[j] & y <= breaks.y[i+1] & y >= breaks.y[i] ) )
+  if (freq) h <- h / n
+  xx <- as.factor(round(mean(breaks.x[1:2])+(0:(nclass-1))*diff(breaks.x[1:2]), 1))
+  yy <- as.factor(round(mean(breaks.y[1:2])+(0:(nclass-1))*diff(breaks.y[1:2]), 1))
+  res <- cbind(expand.grid(xx,yy), h)
+  colnames(res) <- c(deparse(substitute(x)),deparse(substitute(y)),'Frequency')
+  formu <- as.formula(paste("Frequency ~ ", paste(colnames(res)[1:2], collapse= "+")))
+  rotate.cloud(formu, res, panel.3d.cloud=panel.3dbars, col.facet='lightblue', 
+       xbase=1, ybase=1, scales=list(arrows=FALSE, col=1), 
+        par.settings = list(axis.line = list(col = "transparent")),theta=40)
+}
+
+StMartin <- rx
+Echirolles <- ry
+my_hist3d(StMartin, Echirolles, nclass=10)
+
 
 # Recherche de la copule 
 BiCopSelect(Rx/n,Ry/n,familyset=c(1:10), selectioncrit ="AIC",indeptest=FALSE,level=0.05)
