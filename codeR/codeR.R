9d8418146f6eba1138305a0de937ff8ba7b9436e
hashtar <code>
2015-05-01 23:32:41 +0200

    bootstrap gaussienne

--- codeR/codeR.R
+++ codeR/codeR.R
@@ -200,14 +200,85 @@ fitdistr(x,"normal")
 ks.test(x,"pnorm",mean=-0.01599282,sd=0.95044161)
 
 
-####### Copule de Clayton #######
+################################### Copule de Clayton #########################
 
+##############    1) METHODE DES MOMENTS  #######################
+
+#Estimation par la méthode des moments (inversion du tau de Kendall)
+
+tauKendall<-Kendall(x,y)
+paramClayton<-2*tauKendall$tau/(1-tauKendall$tau)
+myClaytonCopula<-claytonCopula(param=paramClayton)
+
+#Khi-plot de la copule de Clayton estimé
+
+number = 1000
+randomEstime<-rCopula(number,myClaytonCopula)
+XClayton<-randomEstime[,1]
+YClayton<-randomEstime[,2]
+Khiplot(XClayton,YClayton,number)
+
+#K-plot de la copule de Clayton estimé
+KplotXRank<-rank(XClayton,ties.method="random")
+KplotYRank<-rank(YClayton,ties.method="random")
+BiCopKPlot(KplotXRank/(number),KplotYRank/(number))
+
+#Bootstrap paramétrique par méthode des moments
+
+Ui = Rx/(n+1)
+Vi = Ry/(n+1)
+
+# copule empirique  bivariée Nelsen, 2006
+uv <- data.frame(Ui,Vi)
+u<-(1:100)/100
+v<-u
+
+fdrEmpirique<-function(u,v){
+  return(EMPIRcop(u,v,para=uv))
+}
+
+UV<-cbind(Ui,Vi)
+N=100
+randomX = NULL
+Dn = sum((fdrEmpirique(Ui,Vi) - pCopula(UV, myClaytonCopula))^2)
+Dnk = NULL
+for (k in 1:N) {
+    print(k)
+    randomXY <-rCopula(n,myClaytonCopula)
+    rankX<-rank(randomXY[,1],ties.method="random")
+    rankY<-rank(randomXY[,2],ties.method="random")
+    Ui = rankX/(n+1)
+    Vi = rankY/(n+1)
+    uv <- data.frame(Ui,Vi)    
+    fdrEmpirique<-function(u,v){
+	return(EMPIRcop(u,v,para=uv))
+    }
+    tauKendall=Kendall(randomXY[,1],randomXY[,2])
+
+    # Estimation par méthode des moments
+    paramClayton<-2*tauKendall$tau/(1-tauKendall$tau)
+    myClaytonCopula<-claytonCopula(param=paramClayton)
+    
+    UV<-cbind(Ui,Vi)
+    Dnk = c(Dnk,sum((fdrEmpirique(Ui,Vi) - pCopula(UV, myClaytonCopula))^2))
+}
+
+alpha = 0.05
+L = sort(Dnk)[floor((1-alpha)*N)]
+#Règle de décision
+Dn > L
+#p-value
+sum(Dnk > Dn)/length(Dnk)
+
+
+##############    2) METHODE CML  #######################
 
 #Estimation du tau de Kendall par la méthode des moments
 tauKendall<-Kendall(x,y)
+paramClayton<-2*tauKendall$tau/(1-tauKendall$tau)
 
 # Estimation semi parametrique (CML) 
-paramClayton<-2*tauKendall$tau/(1-tauKendall$tau)
+
 myClaytonCopula<-claytonCopula(param=paramClayton)
 CML<-fitCopula(data=cbind(Rx/(n+1),Ry/(n+1)),copula=myClaytonCopula,start=paramClayton)
 thetaN<-coef(CML)
@@ -227,7 +298,7 @@ KplotXRank<-rank(XClayton,ties.method="random")
 KplotYRank<-rank(YClayton,ties.method="random")
 BiCopKPlot(KplotXRank/(number),KplotYRank/(number))
 
-#Bootstrap paramétrique
+#Bootstrap paramétrique par CML
 
 Ui = Rx/(n+1)
 Vi = Ry/(n+1)
